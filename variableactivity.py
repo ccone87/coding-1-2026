716acf478d5ce368a6c15e617e1e88ac0995aad2
ccone87 <ccone833@boyslatin.net>
2026-09-02 13:43:41 +0000

    first commit

---
+++ variableactivity.py
@@ -0,0 +1,12 @@
+# variables- a container that stores data
+# name your container almost anything you want, so long
+# as it is descriptive 
+
+name = "carl"
+yearsofexp= 1
+graduate= false 
+
+# when we want to create a variable, we make up a word,
+# use the equal sign and then assign it a data type.
+# whenever the computer sees our variable word it will
+#alway use the data inside
