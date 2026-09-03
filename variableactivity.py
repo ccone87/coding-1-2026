620983f11026b6fd7578d25f125cb346b6f73b77
ccone87 <ccone833@boyslatin.net>
2026-09-03 13:45:00 +0000

    classwork

--- variableactivity.py
+++ variableactivity.py
@@ -10,3 +10,28 @@ graduate= false
 # use the equal sign and then assign it a data type.
 # whenever the computer sees our variable word it will
 #alway use the data inside
+
+# variable naming rules
+# 1. A variable must start with letter and cannot start with a number 
+# 2. A variable can be a mix of letters and numbers but not symbols 
+# 3. A variable cannot be a python keyword
+
+applesinsupermarket= 23
+mathstudentcount= 16
+travelinsuranceontripyes/no 
+platniummembership
+sprite3coke5fanta5
+
+
+# camel case- when creating variable names multiple words,
+# every new word begins with a capital letter 
+# the reason why its called camel case is bc every new word
+# represents a hump.
+usernameForNewInstagramAccounts = 0 
+passwordRecoveryTikTokAccount = 0 
+
+# Snake Case - when creating a variable using snake case
+# we use a underscore symbol to space is bc the underscore
+#r represents a snake on the ground new_word_
+username_for_new_instagram_accounts = 0 
+password_Recovery_TikTok_Account = 0
