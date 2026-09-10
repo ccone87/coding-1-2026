5cb2656a9926c5bc0a1b206c9201f808107a369d
ccone87 <ccone833@boyslatin.net>
2026-09-10 13:39:11 +0000

    operator notes

---
+++ operators.py
@@ -0,0 +1,58 @@
+# assignment operators- exclusively used to 
+# assign values to variables.
+# key / value pairings 
+
+# we use a single equal sign to represent the assignment 
+# operator 
+name = carl 
+grade = 12
+school= true 
+
+# arithmetic operators- used on numerical
+# data types to perform calculations. 
+# intergers (whole numbers) and floats (decimal numbers)
+
+# print is a function that lets us show code 
+# in the terminal
+print(3 + 3) # additional operator
+print(3 - 3) # subtraction operator
+print(12 / 3) # division operator
+print(3 * 4) # multiplaction operator
+
+# comparison operators - set of symbols used 
+# to assess if data is the same or different and
+# how they differ
+
+print(10 > 1) # greater than operator 
+print(10 < 2) # less than operator 
+
+# 2 equal signs compare if something is the same
+print("carl" == "carl") # same as 
+
+# 2 equal signs compare if something is the same 
+print("book" == "book") # same as (false)
+print("2" == 2) # same as (false) not the same datatypes
+print(2.0 == 2) # same as (true)
+
+
+# not equal is written with !=
+# this iis to check and filter for values that are not 
+# the same
+print(200 != 100) # true- these are not the same 
+print(300!= 300) # false these are not the same
+
+# logical operators- compares 2 conditions to check if 
+# they are true or false 
+
+# and - checks if 2 conditions are true. if yes, the final 
+# result true 
+print(3 > 1 and 100 >50) # this would come out to be true
+
+# or- checks if only 1 condition is true. if yes
+# final result will be true 
+print(3 > 1 or 100 > 50)
+
+# not - the "opposite day" operator. it will reserve the 
+# result of the logical operators
+print(not(3 > 1 and 100 > 50) )
+3 this would come out to be false
