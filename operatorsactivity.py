edfe7adcfe2d93b1dac1cba0e97e29e8945d4d4f
ccone87 <ccone833@boyslatin.net>
2026-09-16 13:41:25 +0000

    operator work 2

--- operatorsactivity.py
+++ operatorsactivity.py
@@ -9,3 +9,24 @@ False
 
 #4 print("0" == 0)
 True 
+
+book = 10.99
+tablet = 399.99
+
+print(book + tablet)
+
+cart = 98.87
+amounttogetdiscount = 100.00
+
+print(cart > amounttogetdiscount)
+
+shoes = 200.00
+tax = 0.07
+
+print(shoes * tax)
+
+gpa = 86
+recommendation = false
+
+print(gpa > 85 and recommendation == true)
+
