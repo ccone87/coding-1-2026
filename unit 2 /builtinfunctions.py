72a72b99c475d30ac2ff19d41819706c9fe1c636
ccone87 <ccone833@boyslatin.net>
2026-09-23 13:48:26 +0000

    function work

---
+++ unit 2 /builtinfunctions.py
@@ -0,0 +1,25 @@
+# function - simply put; a code block of
+# instructions for computers to folllow.
+
+# built- in function - a code block of 
+# instruction  for a computer to follow 
+# that was already written for us.
+# pre-written code instructions 
+
+print('Hello')
+year = input(" what year is it")
+print(year)
+
+# input() a built in function that allows a user to
+# type in datam into the terminal. the data that is passed
+# in will always be treated as a string data type. 
+
+# print() a built in function that allows a user to 
+# print data out in the terminal. 
+
+# float() - A function that will change any datatype
+# passed into it, into float/ decimal number
+
+num2 = input("type in a number: ")
+# input always returns a string
+print(9 + float(num2))
---
+++ unit 2 /functionActivity1.py
@@ -0,0 +1,13 @@
+1.
+val = input("please type in a number: ")
+print(int val > 85)
+
+# notes: i need to use input to take in a number
+# comparing some number we type in to 85
+# print true if the number is over 85 
+# print false if it is below 85
+
+
+2. 
+num = input 
+print(type(x))
